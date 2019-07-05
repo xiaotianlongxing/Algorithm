cef43b64b765dac575566f31dd848d700cbb9f7b
xiaotianlongxing <840162388@qq.com>
2019-07-05 23:44:03 +0800

    IsPowerOf2

--- interview/IsPowerOf2.java
+++ interview/IsPowerOf2.java
@@ -1,7 +1,12 @@
 package com.interview;
 
 public class IsPowerOf2 {
-
+    /**
+     * 用O(1)的时间复杂度判断一个数是否是2的N次幂
+     * @param num
+     * @return
+     */
+    
     public static boolean isPowerOf2(int num) {
         return ((num & num - 1) == 0);
     }
